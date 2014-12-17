c39c7f7524b1841c6e5d4794b9e4c9fe57462b95
Ludwig <ludwig.kjellstrom@amnesty.se>
2014-12-17 10:06:41 +0100

    Sync is now capable of syncing more than the 20 latest. Send an integer along with mynewsdesk_syncall to alter the max number of each media type.

--- mynewsdesk/management/commands/mynewsdesk_syncall.py
+++ mynewsdesk/management/commands/mynewsdesk_syncall.py
@@ -2,6 +2,12 @@ from django.core.management.base import BaseCommand, CommandError
 from mynewsdesk import sync
 
 class Command(BaseCommand):
+    args = '[limit]'
+
     def handle(self, *args, **options):
-        r = sync.sync_all()
+        if len(args):
+            r = sync.sync_all(limit=int(args[0]))
+        else:
+            r = sync.sync_all()
+
         print r
--- mynewsdesk/sync.py
+++ mynewsdesk/sync.py
@@ -6,35 +6,35 @@ def add_report(report, result):
     report['errors'] += result['errors']
     return report
 
-def sync_all():
+def sync_all(limit=20):
     report = {
         'created': 0,
         'updated': 0,
         'errors': 0
     }
 
-    r = sync_list(models.TYPE_PRESSRELEASE)
+    r = sync_list(models.TYPE_PRESSRELEASE, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_NEWS)
+    r = sync_list(models.TYPE_NEWS, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_BLOG_POST)
+    r = sync_list(models.TYPE_BLOG_POST, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_CONTACT_PERSON)
+    r = sync_list(models.TYPE_CONTACT_PERSON, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_DOCUMENT)
+    r = sync_list(models.TYPE_DOCUMENT, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_EVENT)
+    r = sync_list(models.TYPE_EVENT, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_IMAGE)
+    r = sync_list(models.TYPE_IMAGE, limit)
     report = add_report(report, r)
 
-    r = sync_list(models.TYPE_VIDEO)
+    r = sync_list(models.TYPE_VIDEO, limit)
     report = add_report(report, r)
 
     return report
@@ -43,71 +43,92 @@ def add_link(material, link):
     db_link = models.Link(material=material, url=link['url'], text=link['text'])
     db_link.save()
 
-def sync_list(service=models.TYPE_PRESSRELEASE):
-    item_list = api.get_list(service)
+def get_total_count(service):
+    "Fetch total_count by sending a minimal api-request."
+    response = api.get_list(type_of_media=service, limit=1)
+    if not response or not 'items' in response.keys() or not 'total_count' in response['items']:
+        count =  0
+    else:
+        count = int(response['items']['total_count'])
+    return count
+
+def fetch_items(service, limit):
+    "Returns a list of all the items available at mynewsdesk"
+    total_count = get_total_count(service)
+    if total_count < limit:
+        limit = total_count
+
+    items = []
+    while len(items) < limit:
+        response = api.get_list(type_of_media=service, offset=len(items), limit=limit)
+        items.extend(response['items']['item'])
+
+    return items
+
+def sync_list(service=models.TYPE_PRESSRELEASE, limit=20):
+    item_list = fetch_items(service, limit)
     report = {
         'created': 0,
         'updated': 0,
         'errors': 0
     }
-    if item_list and 'items' in item_list.keys() and 'item' in item_list['items']:
-        for item in item_list['items']['item']:
+    for item in item_list:
+        try:
             try:
-                try:
-                    db_item = models.Material.objects.get(id=item['id'])
-                    report['updated'] += 1
-                except models.Material.DoesNotExist:
-                    db_item = models.Material(id=item['id'])
-                    report['created'] += 1
+                db_item = models.Material.objects.get(id=item['id'])
+                report['updated'] += 1
+            except models.Material.DoesNotExist:
+                db_item = models.Material(id=item['id'])
+                report['created'] += 1
 
-                for key, val in item.items():
-                    if not isinstance(val, list) and not (type(val) is dict):
-                        setattr(db_item, key, val)
+            for key, val in item.items():
+                if not isinstance(val, list) and not (type(val) is dict):
+                    setattr(db_item, key, val)
 
-                db_item.save()
+            db_item.save()
 
-                for key, val in item.items():
-                    if isinstance(val, list) or (type(val) is dict):
-                        if 'channels' == key:
-                            for channel in val['channel']:
-                                try:
-                                    db_channel = models.Channel.objects.get(id=channel['id'])
-                                except:
-                                    db_channel = models.Channel(id=channel['id'], title=channel['text'])
-                                    db_channel.save()
-                                db_item.channels.add(db_channel)
-                        elif 'event_types' == key:
-                            for event_type in val['event_type']:
-                                try:
-                                    db_event_type = models.EventType.objects.get(id=event_type['id'])
-                                except:
-                                    db_event_type = models.EventType(id=event_type['id'], title=event_type['text'])
-                                    db_event_type.save()
-                                db_item.event_types.add(db_event_type)
-                        elif 'tags' == key:
-                            for tag in val['tag']:
-                                try:
-                                    db_tag = models.Tag.objects.get(name=tag)
-                                except:
-                                    db_tag = models.Tag(name=tag)
-                                    db_tag.save()
-                                db_item.tags.add(db_tag)
+            for key, val in item.items():
+                if isinstance(val, list) or (type(val) is dict):
+                    if 'channels' == key:
+                        for channel in val['channel']:
+                            try:
+                                db_channel = models.Channel.objects.get(id=channel['id'])
+                            except:
+                                db_channel = models.Channel(id=channel['id'], title=channel['text'])
+                                db_channel.save()
+                            db_item.channels.add(db_channel)
+                    elif 'event_types' == key:
+                        for event_type in val['event_type']:
+                            try:
+                                db_event_type = models.EventType.objects.get(id=event_type['id'])
+                            except:
+                                db_event_type = models.EventType(id=event_type['id'], title=event_type['text'])
+                                db_event_type.save()
+                            db_item.event_types.add(db_event_type)
+                    elif 'tags' == key:
+                        for tag in val['tag']:
+                            try:
+                                db_tag = models.Tag.objects.get(name=tag)
+                            except:
+                                db_tag = models.Tag(name=tag)
+                                db_tag.save()
+                            db_item.tags.add(db_tag)
 
-                        elif 'links' == key:
-                            models.Link.objects.filter(material=db_item).delete()
-                            if isinstance(val['link'], list):
-                                for link in val['link']:
-                                    add_link(db_item, link)
-                            else:
-                                add_link(db_item, val['link'])
+                    elif 'links' == key:
+                        models.Link.objects.filter(material=db_item).delete()
+                        if isinstance(val['link'], list):
+                            for link in val['link']:
+                                add_link(db_item, link)
+                        else:
+                            add_link(db_item, val['link'])
 
-                        # TODO: subjects
-                        # TODO: geographic_areas
-                        # TODO: contact_peoples
-                        # TODO: related_items
-                        # TODO: instant_messaging
-                        # TODO: comments
-            except:
-                report['errors'] += 1
+                    # TODO: subjects
+                    # TODO: geographic_areas
+                    # TODO: contact_peoples
+                    # TODO: related_items
+                    # TODO: instant_messaging
+                    # TODO: comments
+        except:
+            report['errors'] += 1
 
     return report
